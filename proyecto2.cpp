e3c8fcd98bb987f62b1e2dc86177e127d6b0f3ac
VeraSupremo <martiniverinirx@gmail.com>
2023-11-14 22:28:30 -0300

    creacion dl proyecto 1
    se dejaron un par de cout para empezar luego se elaborara el menu para poder darle orden a lo que se debera de realizar

---
+++ proyecto2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <windows.h>
+using namespace std;
+
+
+
+
+
+int main(){
+
+
+cout<<"___________________________________________________"<<endl;
+cout<<"________________Bienvenido guardian________________"<<endl;
+Sleep(100);
+cout<<"______________Que opcion desea elegir______________"<<endl;
+
+
+
+
+return 0;
+}
+
