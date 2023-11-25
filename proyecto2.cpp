529c129cd99de5aeb30be67c4914414216c37d84
VeraSupremo <martiniverinirx@gmail.com>
2023-11-25 16:06:58 -0300

    se cambiaran ciertas cosas
    dbeido a que me puse a avanzar en otro codigo para luego actualizar este por ende los cambios en comparacion a este seran sustanciales

--- proyecto2.cpp
+++ proyecto2.cpp
@@ -6,6 +6,10 @@
 #include <string>
 using namespace std;
 
+struct ciudades{
+	string nombre;
+	vector<ciudades* >caminoAdyacente; 
+};
  struct guardian{
 	string nombre;
 	int lvl;
@@ -18,6 +22,14 @@ struct nodo{
 	int DatoNIvel;
 	nodo* izquierda;
 	nodo* derecha;
+};
+struct Genodo{
+	guardian* datos;
+	vector<Genodo*> hijos;
+
+   Genodo(guardian* datos) {
+  		this->datos = datos;
+	}
 };
 nodo* crearNodo(int DatoNIvel){
 	guardian* Aguard= new guardian;
@@ -63,7 +75,7 @@ void imprimir(nodo* raiz){
 //funcion para leer archivos
 
 void LeeGuardianes(const string& nombreArchivo, nodo*& arbol) {
-    ifstream archivo("");
+    ifstream archivo("texx.txt");
 
     if (!archivo.is_open()) {
         cerr << "No se pudo abrir el archivo." << endl;
@@ -73,8 +85,8 @@ void LeeGuardianes(const string& nombreArchivo, nodo*& arbol) {
     while (!archivo.eof()) {
         guardian* nuevoGuardian = new guardian;
 
-        if (archivo >> nuevoGuardian->nombre >> nuevoGuardian->lvl >> nuevoGuardian->maestro >> nuevoGuardian->ciudad) {
-            arbol = agregar(arbol, nuevoGuardian);
+        if (archivo >> nuevoGuardian->nombre >> nuevoGuardian->lvl >> nuevoGuardian->maestro >> nuevoGuardian->ciudad){
+            arbol = agregar(arbol, nuevoGuardian->lvl);
         } else {
             delete nuevoGuardian;  // Liberar memoria si la lectura falla
             cerr << "Error al leer datos desde el archivo." << endl;
@@ -85,9 +97,41 @@ void LeeGuardianes(const string& nombreArchivo, nodo*& arbol) {
     archivo.close();
 }
 
+
+
+
+//creacion arbol general 
+
+Genodo* crearNodo(guardian* datos){
+    return new Genodo(datos);
+}
+
+// Función para agregar un nuevo nodo hijo a un nodo
+void agregarHijo(Genodo* padre,Genodo* hijo){
+    padre->hijos.push_back(hijo);
+}
+// Función para imprimir el árbol general
+void imprimir(Genodo* raiz,int C=0){
+    if(raiz == NULL){
+        return;
+    }
+
+    cout<<string(C, '-')<<" " << raiz->datos->nombre; 
+	cout<< " - Nivel: "<<raiz->datos->lvl<<endl;
+
+   /* for (Genodo* hijo : raiz->hijos) {
+        imprimir(hijo,C + 1);
+    }*/
+}
+
+
+
+
+
 int main(){
 nodo* arbol = NULL;
-LeeGuardianes("",arbol);
+nodo* raiz = NULL;
+LeeGuardianes("texx.txt",arbol);
 
 cout<<"___________________________________________________"<<endl;
 cout<<"________________Bienvenido guardian________________"<<endl;
